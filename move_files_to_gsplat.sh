865eaf91047c4bd787a41bc6f0c154ef51571ebf
Sakura-spring <72288628+Sakura-spring@users.noreply.github.com>
2026-04-07 23:26:59 +0800

    Delete move_files_to_gsplat.sh

--- move_files_to_gsplat.sh
+++
@@ -1,6 +0,0 @@
-gsplat_dir='./gaussian-splatting'
-mv train.py "$gsplat_dir"/
-mv render.py "$gsplat_dir"/
-mv run.sh $gsplat_dir/
-mv elbo.py $gsplat_dir/utils/
-mv loss_utils.py $gsplat_dir/utils/
